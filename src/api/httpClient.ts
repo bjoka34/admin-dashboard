db0f5c5ad87c730d0b4e8c6eb1cab2f227026bd1
Bojan <bjoka34@gmail.com>
2024-06-30 13:27:44 +0200

    api fix

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // export const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const httpClient = axios.create({
-    baseURL: "https://api.meprogram.me/api",
+    baseURL: "https://api.meprogram.me:5044/api",
     // baseURL: "http://localhost:5022/api",
     headers: {
         'Content-Type': 'application/json',
